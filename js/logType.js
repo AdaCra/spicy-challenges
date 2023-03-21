555c7a81d89ab68ad8631c0b9b875243787c4dc1
Adam Hannath <ahannath@gmail.com>
2023-03-21 12:10:51 +0100

    gitignore

--- js/logType.js
+++ js/logType.js
@@ -1,54 +1,68 @@
-let input = [];
-let data;
-if (input == null) {
-  data = null;
-} else if (Array.isArray(input)) {
-  data = "array";
-} else {
-  data = typeof input;
+function testFunction(log) {
+  return;
 }
-console.log("input: ", typeof input, "data: ", typeof data);
+let data;
 
-switch (data) {
-  case "undefined":
-    console.log("undefined!");
-    break;
-  case null:
-    console.log("null!");
-    break;
-  case "number":
-    console.log("number!");
-    break;
-  case "string":
-    console.log("string!");
-    break;
-  case "boolean":
-    console.log("boolean!");
-    break;
-  case "bigint":
-    console.log("bigint!");
-    break;
-  case "function":
-    console.log("function!");
-    break;
-  case "object":
-    console.log("object!");
-    break;
-  case "array":
-    console.log("array!");
-    break;
-  default:
-    console.log("I have no idea!");
+let input = [
+  undefined,
+  null,
+  NaN,
+  ["array"],
+  42,
+  "42",
+  true,
+  42n,
+  testFunction(42),
+  { number: 42 },
+];
+for (let i = 0; i < input.length; i++) {
+  data = input[i];
+  console.log(data);
+  switch (true) {
+    case data === undefined:
+      console.log("undefined!");
+      break;
+    case data === null:
+      console.log("null!");
+      break;
+    case Number.isNan(data):
+      console.log("not a number!");
+      break;
+    case Array.isArray(data):
+      console.log("array!");
+      break;
+    case typeof data === "number":
+      console.log("number!");
+      break;
+    case typeof data === "string":
+      console.log("string!");
+      break;
+    case typeof data === "boolean":
+      console.log("boolean!");
+      break;
+    case typeof data === "bigint":
+      console.log("bigint!");
+      break;
+    case typeof data === "function":
+      console.log("function!");
+      break;
+    case typeof data === "object":
+      console.log("object!");
+      break;
+    default:
+      console.log("I have no idea!");
+      break;
+  }
 }
 
 // ("undefined!");
 // ("null!");
+// ("array!");
 // ("number!");
+// ("string!"
 // ("not a number!");
-// ("string!");
 // ("boolean!");
 // ("bigint!");
 // ("function!");
 // ("object!");
-// ("array!");
 // ("I have no idea!");
