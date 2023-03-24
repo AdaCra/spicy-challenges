bf3cdd9fae31fa353c588f60512f4eced102556f
Adam Hannath <ahannath@gmail.com>
2023-03-24 11:37:17 +0100

    I'm a Toast Message

--- js/7_DOM-create-elements/1_toast-messages/js/index.js
+++ js/7_DOM-create-elements/1_toast-messages/js/index.js
@@ -6,8 +6,20 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  console.log("TEST THAT IT WORKS!");
+  let newListItem = document.createElement("li");
+  toastContainer.append(newListItem);
+  // toastContainer.append(document.createElement("li"));
+  document
+    .querySelector('[data-js="toast-container"]')
+    .append(document.createElement("li"));
+  newListItem.classList.add(`toast-container__message`);
+  newListItem.innerText = `I'm a toast message`;
+
+  // `<li class='toast-container__message'>I'm a toast message.</li>`;
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
