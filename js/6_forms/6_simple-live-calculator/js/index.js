03a2816f072a76b20bf0ca35ba5544af59820a71
Adam Hannath <ahannath@gmail.com>
2023-03-24 17:02:26 +0100

    simple calc

--- js/6_forms/6_simple-live-calculator/js/index.js
+++ js/6_forms/6_simple-live-calculator/js/index.js
@@ -3,3 +3,17 @@ console.clear();
 const firstInput = document.querySelector('[data-js="firstValue"]');
 const secondInput = document.querySelector('[data-js="secondValue"]');
 const result = document.querySelector('[data-js="result"]');
+
+firstInput.addEventListener("input", (event) => {
+  let number1 = parseInt(firstInput.value);
+  let number2 = parseInt(secondInput.value);
+  let calculation = number1 * number2;
+  result.textContent = `${calculation}`
+});
+
+secondInput.addEventListener("input", (event) => {
+  let number1 = parseInt(firstInput.value);
+  let number2 = parseInt(secondInput.value);
+  let calculation = number1 * number2;
+  result.textContent = `${calculation}`
+});
