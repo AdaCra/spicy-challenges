4ed1d6999acda76d8703f3d9cc2344cb42b9a09a
Adam Hannath <ahannath@gmail.com>
2023-03-24 16:44:18 +0100

    to Uppercase

--- js/6_forms/5_controlled-input-uppercase/js/index.js
+++ js/6_forms/5_controlled-input-uppercase/js/index.js
@@ -1,3 +1,8 @@
 console.clear();
 
 const input = document.querySelector('[data-js="toUpperCase"]');
+
+input.addEventListener("input", (event) => {
+  let inputKey = input.value;
+  input.value = inputKey.toUpperCase();
+});
