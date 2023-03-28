6137a5400ba5583085067e6d9404e56c0fa1bc73
Adam Hannath <ahannath@gmail.com>
2023-03-28 16:09:44 +0200

    task 5 part 1 stars complete

--- js/8_Loops/5_stars/js/index.js
+++ js/8_Loops/5_stars/js/index.js
@@ -7,6 +7,13 @@ function renderStars() {
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
+  for (let i = 1; i <= 5; i++) {
+    console.log(i);
+    const newStar = document.createElement("img");
+    newStar.src = "assets/star-empty.svg";
+    newStar.setAttribute("src", "assets/star-empty.svg");
+    starContainer.appendChild(newStar);
+  }
 
   //--^-- your code here --^--
 }
