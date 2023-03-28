4cbd02a2534df5da1335e7f6a1c3968ca617830d
Adam Hannath <ahannath@gmail.com>
2023-03-28 16:42:35 +0200

    task 5 stars complete

--- js/8_Loops/5_stars/js/index.js
+++ js/8_Loops/5_stars/js/index.js
@@ -2,20 +2,25 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
   for (let i = 1; i <= 5; i++) {
-    console.log(i);
     const newStar = document.createElement("img");
-    newStar.src = "assets/star-empty.svg";
-    newStar.setAttribute("src", "assets/star-empty.svg");
+    newStar.setAttribute("data-js", "starry_night");
+    if (i <= filledStars) {
+      newStar.src = "assets/star-filled.svg";
+    } else {
+      newStar.src = "assets/star-empty.svg";
+    }
+    newStar.addEventListener("click", ()=>{
+      renderStars(i)
+    })
     starContainer.appendChild(newStar);
   }
 
   //--^-- your code here --^--
 }
-
 renderStars();
