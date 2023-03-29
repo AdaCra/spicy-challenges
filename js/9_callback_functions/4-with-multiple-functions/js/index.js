3ffcfecc4077ffd997809ab6dc6914b3511a3655
Adam Hannath <ahannath@gmail.com>
2023-03-29 11:01:27 +0200

    intial commit, task  complete

--- js/9_callback_functions/4-with-multiple-functions/js/index.js
+++ js/9_callback_functions/4-with-multiple-functions/js/index.js
@@ -4,8 +4,22 @@ function handleUserLogin(onSuccess, onError, userName) {
   if (userName === "Jane Doe") {
     onSuccess(userName, "admin");
   } else {
-    onError(`Unknown user "${userName}"`);
+    onError(`User "${userName}" does not exist`);
   }
 }
 
 // The exercise starts here!
+function showWelcomeMessage(userName, userRole) {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
+}
+
+function showErrorMessage(errorMessage) {
+  console.log(`Login error: ${errorMessage}`);
+}
+
+let currentUser = "Jane Doe";
+
+handleUserLogin(showWelcomeMessage, showErrorMessage, currentUser);
+
+currentUser = "John Doe";
+handleUserLogin(showWelcomeMessage, showErrorMessage, currentUser);
