f8eb148749af6598bf0bdd6cdc44cbb688890a8f
Adam Hannath <ahannath@gmail.com>
2023-04-05 15:30:06 +0200

    initial commit & task 1 get animals complete

---
+++ js/16_js-unit-testing/01_get-animal/index.js
@@ -0,0 +1,7 @@
+export function getAnimal(string) {
+ if (string === "cats") {
+    return "I totally love cats!"
+ } else if (string === "dogs") {
+    return "I like dogs!"
+ } return "I do not like animals at all!"
+}
