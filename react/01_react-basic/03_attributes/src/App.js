ab0f49dcefab38b4e53c69adddd7fb9ac18e95cc
Adam Hannath <ahannath@gmail.com>
2023-04-11 15:43:36 +0200

    task 3 - react attributes complete

--- react/01_react-basic/03_attributes/src/App.js
+++ react/01_react-basic/03_attributes/src/App.js
@@ -2,5 +2,17 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <RunThisShit/>;
+}
+
+
+export const RunThisShit = () => {
+  return (
+    <article>
+      <h2 className="article__title">Neden aynı şeyleri tekrar tekrar yapmaya devam ediyorum?</h2>
+      <label htmlFor="article_input">Önceki girişi tekrar dene:</label>
+      <input id="article_input" placeholder="Bunun nasıl çalıştığını biliyorsun! Sadece yap, şimdiden!"></input>
+      <a className="article__link" href="https://www.turkceterapi.com/blog/kendinden-nefret-etmekkendini-sevememek">Yardım etmenin bir yolunu bul.</a>
+    </article>
+  )
 }
