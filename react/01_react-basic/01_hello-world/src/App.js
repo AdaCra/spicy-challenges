497f285f43864e8103dacf087ee479500cce0fc1
Adam Hannath <ahannath@gmail.com>
2023-04-11 15:13:45 +0200

    task 1 - hello world complete

--- react/01_react-basic/01_hello-world/src/App.js
+++ react/01_react-basic/01_hello-world/src/App.js
@@ -2,5 +2,9 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Say hello...</div>;
+  return <HelloWorld />;
 }
+
+export const HelloWorld = () => {
+  return <h1>Hallo Weld</h1>;
+};
