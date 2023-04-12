6714c3cc5ff2032e842604dc4b20c9e9afe0c1c0
Adam Hannath <ahannath@gmail.com>
2023-04-12 11:57:30 +0200

    task 3 smiley complete

--- react/02_react-props/03_smiley/src/App.js
+++ react/02_react-props/03_smiley/src/App.js
@@ -2,5 +2,9 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Smiley isHappy />;
 }
+
+export const Smiley = ({ isHappy }) => {
+  return <h1>{isHappy ? "😊" : "😫"}</h1>;
+};
