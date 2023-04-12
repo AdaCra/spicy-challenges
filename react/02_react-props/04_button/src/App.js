166578edde22539f1398c9a07c439b1504bf12e3
Adam Hannath <ahannath@gmail.com>
2023-04-12 14:03:18 +0200

    buttons done

--- react/02_react-props/04_button/src/App.js
+++ react/02_react-props/04_button/src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <>
+      <Button color={"#ef0404"} text={"This Button is ON"} />
+      <Button color={"#001dff"} disabled text={"This Button is OFF"} />
+    </>
+  );
 }
+
+export const Button = ({ color, disabled, text }) => {
+  return (
+    <button
+      disabled={disabled}
+      style={{ backgroundColor: color }}
+      onClick={() => alert("You clicked me!?")}
+    >
+      {text}
+    </button>
+  );
+};
