1f8294f63ea47d5632a406c5399d8f4389e6ee39
Adam Hannath <ahannath@gmail.com>
2023-04-14 16:07:02 +0200

    task 2

--- react/06_react-state-2/02_table-reservation/src/App.js
+++ react/06_react-state-2/02_table-reservation/src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./components/Counter";
 import "./styles.css";
 
 export default function App() {
+  
+  const [people, setPeople] = useState(0);
+  const Bob = (value) => {
+    setPeople(people + value);
+  };
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter People={Bob} />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
