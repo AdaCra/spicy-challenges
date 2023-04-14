78bd259b151cfbb37c6507675250ff228c732d36
Adam Hannath <ahannath@gmail.com>
2023-04-14 11:35:15 +0200

    task 1 complete

--- react/05_react-with-arrays/01_fruits/src/App.js
+++ react/05_react-with-arrays/01_fruits/src/App.js
@@ -1,11 +1,38 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
-
+  const fruits = [
+    {
+      id: 1337,
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: 1999,
+      name: "🍇 Grape",
+      color: "purple",
+    },
+    {
+      id: 1865,
+      name: "🍊 Orange",
+      color: "orange",
+    },
+    {
+      id: 1542,
+      name: "🍐 Pear",
+      color: "green",
+    },
+    {
+      id: 1295,
+      name: "🍎 Apple",
+      color: "red",
+    },
+  ];
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit) => (
+        <Card key={fruit.id} fruit={fruit} />
+      ))}
     </div>
   );
 }
