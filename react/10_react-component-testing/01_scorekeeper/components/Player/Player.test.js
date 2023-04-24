09611d1430dc8adac91eb3015c9f7f7276804584
Adam Hannath <ahannath@gmail.com>
2023-04-24 13:55:00 +0200

    player test complete

--- react/10_react-component-testing/01_scorekeeper/components/Player/Player.test.js
+++ react/10_react-component-testing/01_scorekeeper/components/Player/Player.test.js
@@ -2,6 +2,39 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Player from ".";
 
-test("renders player information and two buttons", () => {});
+test("renders player information and two buttons", () => {
+  render(<Player name="bob" score={0} />);
+  const buttons = screen.getAllByRole("button");
+  const nameLabel = screen.getByText("bob");
 
-test("calls callbacks when increasing or decreasing score", async () => {});
+  expect(buttons).toHaveLength(2);
+  expect(nameLabel).toBeInTheDocument();
+});
+
+test("calls callbacks when increasing or decreasing score", async () => {
+  const handleDecrease = jest.fn();
+  const handleIncrease = jest.fn();
+
+  const user = userEvent.setup();
+
+  render(
+    <Player
+      name="bob"
+      score={0}
+      onDecreasePlayerScore={handleDecrease}
+      onIncreasePlayerScore={handleIncrease}
+    />
+  );
+  const buttons = screen.getAllByRole("button");
+  const decreaseButton = buttons[0];
+  const increaseButton = buttons[1];
+
+  await user.click(increaseButton);
+  await user.click(increaseButton);
+  await user.click(decreaseButton);
+  await user.click(decreaseButton);
+  await user.click(decreaseButton);
+
+  expect(handleIncrease).toHaveBeenCalledTimes(2);
+  expect(handleDecrease).toHaveBeenCalledTimes(3);
+});
