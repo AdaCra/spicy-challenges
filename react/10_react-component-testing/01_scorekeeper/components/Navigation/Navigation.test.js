95a59e5a12f8d9c2cfe6a16d80b6fdc8937d6446
Adam Hannath <ahannath@gmail.com>
2023-04-24 11:06:19 +0200

    react-component testing

---
+++ react/10_react-component-testing/01_scorekeeper/components/Navigation/Navigation.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import Navigation from ".";
+
+jest.mock("next/router", () => ({
+  useRouter() {
+    return { pathname: jest.fn() };
+  },
+}));
+
+test("renders with two links 'Play' and 'History'", () => {
+
+  render(<Navigation players={[
+    { name: "John", score: 2, id: "xyz" },
+    { name: "Jane", score: 1, id: "abc" },
+  ]}/>)
+
+  const playerLink = screen.getByRole("link", { name: "Play" });
+  const historyLink = screen.getByRole("link", { name: "History" });
+  
+  expect(playerLink).toBeInTheDocument();
+  expect(historyLink).toBeInTheDocument();
+});
