4b4d3478593e8044e3468bcdccba2d6b4d16d10d
Adam Hannath <ahannath@gmail.com>
2023-05-04 10:39:53 +0200

    task 1 and 2 complete

--- backend/01_backend-basics/02_routes/index.js
+++ backend/01_backend-basics/02_routes/index.js
@@ -1,3 +1,5 @@
-// import { server } from "./server.js";
-
-// server.listen(…
+import { server } from "./server.js";
+const PORT = 8000;
+server.listen(PORT, () => {
+  console.log("It's aliiiiive!")
+});
