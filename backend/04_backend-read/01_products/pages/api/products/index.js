051712d08406555b3aa7c6ffc1587470e93597d7
Adam Hannath <ahannath@gmail.com>
2023-05-08 16:22:41 +0200

    backend Read - Complete

--- backend/04_backend-read/01_products/pages/api/products/index.js
+++ backend/04_backend-read/01_products/pages/api/products/index.js
@@ -6,9 +6,26 @@ export default async function handler(request, response) {
   await dbConnect();
 
   if (request.method === "GET") {
-    const products = await Product.find();
-    return response.status(200).json(products);
-  } else {
-    return response.status(405).json({ message: "Method not allowed" });
+    try {
+      const products = await Product.find();
+      return response.status(200).json(products);
+    } catch (error) {
+      console.error(error);
+      return response
+        .status(405)
+        .json({ message: "Method not allowed", error: error.message });
+    }
+  } else if (request.method === "POST") {
+    try {
+      const productData = request.body;
+      const product = new Product(productData);
+      await product.save();
+      return response.status(201).json({ status: "Product created." });
+    } catch (error) {
+      console.error(error);
+      return response
+        .status(400)
+        .json({ message: "Bad Request", error: error.message });
+    }
   }
 }
